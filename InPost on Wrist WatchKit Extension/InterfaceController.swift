837ad0f1779edc7126fe9bde267a25ef9cb009bf
Konrad Zdunczyk <konrad.zdunczyk@gmail.com>
2020-05-26 18:24:01 +0200

    Initial Commit

---
+++ InPost on Wrist WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  InPost on Wrist WatchKit Extension
+//
+//  Created by Konrad Zdunczyk on 26/05/2020.
+//  Copyright © 2020 Konrad Zdunczyk. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
